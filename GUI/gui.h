bd57d2fb85d2ec53e7b1ad4d2ed2e82195f54c61
Masaomida <2538741436@qq.com>
2022-05-17 20:22:21 +0800

    initiate GUI

---
+++ GUI/gui.h
@@ -0,0 +1,11 @@
+//
+// Created by DELL on 2022/5/17.
+//
+
+#ifndef HIGH_PRECISION_REAL_NUMBER_LIBRARY_GUI_H
+#define HIGH_PRECISION_REAL_NUMBER_LIBRARY_GUI_H
+
+//这地方就写你的图形化界面的函数就好了
+//接下来看看咋push
+
+#endif //HIGH_PRECISION_REAL_NUMBER_LIBRARY_GUI_H
