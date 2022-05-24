d37854da2dcc717fec356c35a956a8e6ae797aec
seulAuguestine <446433655@qq.com>
2022-05-24 13:14:10 +0800

    add

--- Utils/sepBigNumIn.h
+++ Utils/sepBigNumIn.h
@@ -4,9 +4,19 @@
 
 #ifndef HIGH_PRECISION_REAL_NUMBER_LIBRARY_SEPBIGNUMIN_H
 #define HIGH_PRECISION_REAL_NUMBER_LIBRARY_SEPBIGNUMIN_H
+#define MAXSIZE 500
+#include <string.h>
+
+struct FloatBigNum {//超高精度实数
+    int lengthIntager;//表示数据的位数
+    int lengthDecimal;//表示数据的位数
+    char intager[MAXSIZE];//整数部分
+    char decimal[MAXSIZE];//小数部分
+    int flag;//表示符号，-1则为负数、1则为正数、0则表示原数为0
+};
 
 char  *sepBigNumIn (char *x) {//输入带符号的实数 输出整数部分
-    FloatBigNum FBN;
+    struct FloatBigNum FBN;
     int j, k, len;
     int i;
     len = strlen(x);
