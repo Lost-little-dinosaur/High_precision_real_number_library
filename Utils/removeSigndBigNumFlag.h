25d0f79b1253ba79edd8d057882ccc948ac5ec54
seulAuguestine <446433655@qq.com>
2022-05-24 14:39:42 +0800

    add

--- Utils/removeSigndBigNumFlag.h
+++ Utils/removeSigndBigNumFlag.h
@@ -13,15 +13,20 @@ struct SigndBigNum {//有符号大整数
     int flag;//表示符号，-1则为负数、1则为正数、0则表示原数为0
 };
 
-char *removeSigndBigNumFlag(char x[]) {//移除有符号大整数的符号位得到numbody
-    struct SigndBigNum SBN;
+struct UnsigndBignNum {//无符号大整数
+    int length;//表示数据的位数
+    char numBody[MAXSIZE];//表示数据主体
+};
+
+struct UnsigndBignNum removeSigndBigNumFlag(char x[]) {//移除有符号大整数的符号位得到numbody
+    struct UnsigndBignNum SBN;
     SBN.length = strlen(x);
     //printf("%d",SBN.length);
     for (int i = 1, j = 0; i < SBN.length, j < SBN.length - 1; ++i, ++j) {
         SBN.numBody[j] = x[i];
         //printf("%c",SBN.numBody[j]);
     }
-    return SBN.numBody;
+    return SBN;
 }
 
 #endif //HIGH_PRECISION_REAL_NUMBER_LIBRARY_REMOVESIGNDBIGNUMFLAG_H
