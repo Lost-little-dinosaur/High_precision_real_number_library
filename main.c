150906206ffe6c6460190aee86e725e0d7dd3061
Dinosaur <3144794112@qq.com>
2022-05-27 09:07:12 +0800

    refactor project

--- main.c
+++ main.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
-#include "Compute/multiplyUnsigndBignNum.h"
-#include "Compute/UnsignedBigNum.h"
+
 
 int main() {
-    struct UnsignedBigNum a;
-    struct UnsignedBigNum b;
-
-    a.flag = 1;
-    b.flag = 1;
-    strcpy(a.numBody, "123");
-    strcpy(b.numBody, "1234");
-    a.length = 3;
-    b.length = 4;
-//    printf("%s", multiplyUnsigndBigNum(a, b).numBody);
-    test();
+    //这边可以写你们自己测试的代码，但就是别推上来了
     return 0;
 }
