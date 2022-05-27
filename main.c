9bdb5eb2012e7a9dc19a8e234ab2e8e21ace3fe3
Dinosaur <3144794112@qq.com>
2022-05-27 09:51:05 +0800

    refactor main.c

--- main.c
+++ main.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
-#include "newCompute/multiplication.h"
-#include "string.h"
-#include "Model/struct.h"
+
 
 int main() {
-    struct UnsignedBigNum a;
-    struct UnsignedBigNum b;
-
-    a.flag = 1;
-    b.flag = 1;
-    strcpy(a.numBody, "123");
-    strcpy(b.numBody, "1234");
-    a.length = 3;
-    b.length = 4;
-    printf("%s", multiplyUnsignedBigNum(a, b).numBody);
+    //这边放你们自己测试的代码，但就是别推上来了
     return 0;
 }
