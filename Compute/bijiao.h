f1ac2955a9fc850adad4197d0a5a9bb827098d86
Masaomida <2538741436@qq.com>
2022-05-29 18:51:48 +0800

    比较

--- Compute/bijiao.h
+++ Compute/bijiao.h
@@ -10,78 +10,36 @@
 #ifndef HIGH_PRECISION_REAL_NUMBER_LIBRARY_MULTIPLYUNSIGNBIGNUM
 #define HIGH_PRECISION_REAL_NUMBER_LIBRARY_MULTIPLYUNSIGNBIGNUM
 
-=======
-#endif //HIGH_PRECISION_REAL_NUMBER_LIBRARY_BIJIAO_H
 
-#include<stdio.h>
-#include<string.h>
-
-int main() {
-    char Num1[10000];
-    char Num2[10000];
-
-    scanf("%s %s", Num1, Num2);
-    /* check1(Num1);
-      check2(Num2);*/
-    int length1 = Scanf(Num1);
-    int length2 = Scanf(Num2);
-    calculate(Num1, Num2);
-    return 0;
-}
-
-/*void check1(char num1[]){
-    int len=strlen(num1);
-    int i;
-    for(i=0; i<len; i++){
-        if((num1[i]<0||num1[i]>9)&&num1[i]!="."){
-        printf("输入格式有误,请重新输入第一个大数\n");
-        scanf("%s",num1);
-        len=strlen(num1);}
-    }
-}
-void check2(char num2[]){
-    int len=strlen(num2);
-    int i;
-    for(i=0; i<len; i++){
-        if((num2[i]<0||num2[i]>9)&&num2[i]!="."){
-        printf("输入格式有误,请重新输入第二个大数\n");
-        scanf("%s ",num2);
-         len=strlen(num2);}
-    }
+struct SigndBigNum {//有符号大整数
+    int length;//表示数据的位数
+    char numBody[MAXSIZE];//表示数据主体
+    int flag;//表示符号，-1则为负数、1则为正数、0则表示原数为0
+};
 
 #endif //HIGH_PRECISION_REAL_NUMBER_LIBRARY_STRUCT_H
 
-struct SigndBigNum totalcompare( SigndBigNum x,SigndBigNum y){
-    if(x.flag>y.flag){
+struct SigndBigNum totalcompare( struct SigndBigNum x,struct SigndBigNum y) {
+    if (x.flag > y.flag) {
         return x;
-    }
-    else if(x.flag<y.flag){
+    } else if (x.flag < y.flag) {
         return y;
+    } else {
+        calculate(x, y);
     }
-    else{
-        calculate(x,y);
-    }
-
+}
     int main() {
         char a[1005],b[1005];
-        SigndBigNum x,y,r;
-        UnsigndBignNum c,d;
+        struct SigndBigNum x,y;
+       // UnsigndBignNum c,d;
         scanf("%s",&a);
         scanf("%s",&b);
-        x.flag= judgeSigndBigNum(a);
         y.flag= judgeSigndBigNum(b);
-
-        //printf("flag=%d %d\n",x.flag,y.flag);
-
-        x.length= strlen(a)-1;
-        y.length= strlen(b)-1;
-        c=removeSigndBigNumFlag(a);
-        d= removeSigndBigNumFlag(b);
-        strcpy(x.numBody,c.numBody);
-        strcpy(y.numBody,d.numBody);
-
-        /* printf("%s ",x.numBody);
-         printf("%s \n",y.numBody);*/
+        x.length= gps(a);
+        y.length= gps(b);
+        x.numBody=removeSigndBigNumFlag(a);
+        y.numBody= removeSigndBigNumFlag(b);
+        x.flag= judgeSigndBigNum(a);//小伙伴写的函数
 
 
     }
@@ -95,81 +53,81 @@ struct SigndBigNum totalcompare( SigndBigNum x,SigndBigNum y){
                 break;
         return i;
     }
-    char calculate(SigndBigNum x,SigndBigNum y/*char num1[],char num2[]*/)
-=======
-}
-*/
-int Scanf(char num[]) {
-    int len = strlen(num);
-    int i;
-    for (i = 0; i < len; i++)
-        if (num[i] == '.')
-            break;
-    return i;
-}
+    char calculate(struct SigndBigNum x,struct SigndBigNum y/*char num1[],char num2[]*/) {
 
-void calculate(char num1[], char num2[]) {
-    int length1 = Scanf(num1);
-    int length2 = Scanf(num2);
-    if (length1 > length2) {
-        printf("第一个数大于第二个数\n");
-    } else if (length1 < length2) {
-        printf("第二个数大于第一个数\n");
-    } else if (length1 = length2) {
-        zhuwei(num1, num2, length1);
+        if (x.flag > 0) {
+
+            if (x.length > y.length) {
+                return x;
+                else if (x.length < y.length) {
+                    return y;
+                    else if (x.length = y.length) {
+                        zhuwei(x, y);
+                    }
+                }
+            }
+        } else if (x.flag < 0) {
+
+            if (x.length < y.length) {
+                return x;
+                else if (x.length > y.length) {
+                    return y;
+                    else if (x.length1 = y.length) {
+                        zhuwei(x, y);
+                    }
+                }
+            }
+        }
     }
-}
 
 
-int zhuwei(char num1[], char num2[], int length) {
-    int i, j;
-    for (i = 0; i < length; i++) {
-        if (num1[i] != num2[i])
-            break;
+    int zhuwei(struct SigndBigNum x,struct SigndBigNum y){
+                int i,j;
+                for(i=0; i<x.length; i++)
+                {
+                    if(x.numBody[i]!=y.numBody[i])
+                        break;
+                }
+                int sum=x.numBody[i]-y.numBody[i];
+                if(sum * x.flag>0){
+                    return x;
+                }
+                else if(sum * x.flag<0){
+                    return y;
+                }
+                else if(sum=0){
+                    return x;
+                }
+
     }
-    int sum = num1[i] - num2[i];
-    if (sum > 0) {
-        printf("第一个数大于第二个数\n");
-    } else if (sum < 0) {
-        printf("第二个数大于第一个数\n");
-    } else if (sum = 0) {
-        printf("两数相等\n");
-    }
-}
-
 #include<stdio.h>
 #include<string.h>
+            int gps(char num[])//找到小数点所在的位置
+            {
+                struct SigndBigNum a;
 
-int gps(char num[])//找到小数点所在的位置
-{
-    int len = strlen(num);
-    int i;
-    for (i = 0; i < len; i++)
-        if (num[i] == '.')
-            break;
-    return i;
-}
-
-int cmp1(char num1[], char num2[])//比较小数点的大小
-{
-    int len = gps(num1);
-    return strcmp(num1 + len, num2 + len);
-}
-
-int cmp(char num1[], char num2[], int len)//比较大数的大小
-{
-    int i;
-    for (i = 0; i < len; i++) {
-        if (num1[i] != num2[i])
-            break;
-    }
-    int sum = num1[i] - num2[i];
-    if (i != len)
-        return sum;
-    return 0;
-}
-/*int main()
-{
-    char num1[10000];
-    char num2[10000];
-    while(scanf("%s %s",num1,num2)!=EOF)
+                int len=strlen(num);
+                int i;
+                for(i=0; i<len; i++)
+                    if(num[i]=='.')
+                        break;
+                return i;
+            }
+            int cmp1(char num1[],char num2[])//比较小数点的大小
+            {
+                int len=gps(num1);
+                return strcmp(num1+len,num2+len);
+            }
+           /* int cmp(char num1[],char num2[],int len)//比较大数的大小
+            {
+                int i;
+                for(i=0; i<len; i++)
+                {
+                    if(num1[i]!=num2[i])
+                        break;
+                }
+                int sum=num1[i]-num2[i];
+                if(i!=len)
+                    return sum;
+                return 0;
+            }*/
