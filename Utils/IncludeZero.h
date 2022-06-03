c91572fe0c5d1a9caec02500de60bf88e9976b49
seulAuguestine <446433655@qq.com>
2022-06-03 10:03:27 +0800

    adjust

--- Utils/IncludeZero.h
+++ Utils/IncludeZero.h
@@ -8,5 +8,5 @@
 struct UnsignedBigNum IncludeZero (struct UnsignedBigNum x, struct UnsignedBigNum y);
 struct SignedBigNum IncludeZeroSPlus(struct SignedBigNum x,struct SignedBigNum y);
 struct SignedBigNum IncludeZeroSignedSub(struct SignedBigNum x,struct SignedBigNum y);
-
+int cmpUnsignedBigNum(UnsignedBigNum x, UnsignedBigNum y);
 #endif //HIGH_PRECISION_REAL_NUMBER_LIBRARY_INCLUDEZERO_H
