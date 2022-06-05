aaf108128e26b15b5660c48fc28f50cdf7b1a42c
seulAuguestine <446433655@qq.com>
2022-06-05 22:09:17 +0800

    add type turn

--- Utils/Utils.c
+++ Utils/Utils.c
@@ -48,7 +48,7 @@ int judgeSignedBigNum(char x[]) {//判断有符号整数是正数负数还是0
     return SBN.flag;
 }
 
-struct UnsignedBigNum removeSigndBigNumFlag(char x[]) {//移除有符号大整数的符号位得到numbody
+struct UnsignedBigNum removeSignedBigNumFlag(char x[]) {//移除有符号大整数的符号位得到numbody
     struct UnsignedBigNum SBN;
     memset(SBN.numBody, 0, sizeof(SBN.numBody));
     SBN.length = strlen(x) - 1;
@@ -147,8 +147,87 @@ int *judgeType(char x[], char y[]) {  //判断数据类型 如果type[0] type[1]
     } else if (y[0] == '0' && x[0] != '0') {
         type[1] = type[0];
     }
-
-
-
     return type;
+}
+
+struct SignedBigNum unToSigned(int type[],char x[],char y[])//无符号转有符号 输出转换后的结构体
+{
+    struct SignedBigNum a, b;
+    memset(a.numBody, 0, sizeof(a.numBody));
+    memset(b.numBody, 0, sizeof(b.numBody));
+    if (type[0] == 1 && type[1] == 2)//x:un y:sn
+    {
+        strcpy(a.numBody, x);
+        a.flag = 1;
+        a.length = strlen(x);
+        return a;
+    } else if (type[1] == 1 && type[0] == 2)//x:sn y:un
+    {
+        strcpy(b.numBody, y);
+        b.flag = 1;
+        b.length = strlen(y);
+        return b;
+    }
+}
+
+struct FloatBigNum unToFloat(int type[],char x[],char y[])//无符号转浮点 输出转换后的结构体
+{
+    struct FloatBigNum a, b;
+    memset(a.integer, 0, sizeof(a.integer));
+    memset(a.decimal, 0, sizeof(a.decimal));
+    memset(b.integer, 0, sizeof(b.integer));
+    memset(b.decimal, 0, sizeof(b.decimal));
+    if (type[0] == 1 && type[1] == 3)//x:un y:fn
+    {
+        strcpy(a.integer, x);
+        strcpy(a.decimal, "0");
+        a.flag = 1;
+        a.lengthInteger = strlen(x);
+        a.lengthDecimal = 0;
+        return a;
+    } else if (type[1] == 1 && type[0] == 3)//x:fn y:un
+    {
+        strcpy(b.integer, y);
+        strcpy(b.decimal, "0");
+        b.flag = 1;
+        b.lengthInteger = strlen(y);
+        b.lengthDecimal = 0;
+        return b;
+    }
+}
+
+struct FloatBigNum snToFloat(int type[],char x[],char y[])//有符号转浮点 输出转换后的结构体
+{
+    struct FloatBigNum a, b;
+    SignedBigNum c, d;
+    UnsignedBigNum e, f;
+    memset(c.numBody, 0, sizeof(c.numBody));
+    memset(d.numBody, 0, sizeof(d.numBody));
+    memset(e.numBody, 0, sizeof(e.numBody));
+    memset(f.numBody, 0, sizeof(f.numBody));
+    memset(a.integer, 0, sizeof(a.integer));
+    memset(a.decimal, 0, sizeof(a.decimal));
+    memset(b.integer, 0, sizeof(b.integer));
+    memset(b.decimal, 0, sizeof(b.decimal));
+    if (type[0] == 2 && type[1] == 3)//x:sn y:fn
+    {
+        c.flag = judgeSignedBigNum(x);
+        a.flag = c.flag;
+        e = removeSignedBigNumFlag(x);
+        strcpy(a.integer, e.numBody);
+        strcpy(a.decimal, "0");
+        a.lengthInteger = strlen(e.numBody);
+        a.lengthDecimal = 0;
+        return a;
+    } else if (type[1] == 2 && type[0] == 3)//x:fn y:sn
+    {
+        d.flag = judgeSignedBigNum(y);
+        b.flag = d.flag;
+        f = removeSignedBigNumFlag(x);
+        strcpy(b.integer, f.numBody);
+        strcpy(b.decimal, "0");
+        b.lengthInteger = strlen(f.numBody);
+        b.lengthDecimal = 0;
+        return b;
+    }
 }
--- Utils/Utils.h
+++ Utils/Utils.h
@@ -8,10 +8,13 @@
 char addSignedBigNumFlag(struct SignedBigNum x);
 int judgeFloatBigNum (char x[]);
 int judgeSignedBigNum (char x[]);
-struct UnsignedBigNum removeSigndBigNumFlag(char x[]);
+struct UnsignedBigNum removeSignedBigNumFlag(char x[]);
 char  *sepBigNumDe (char *x);
 char  *sepBigNumIn (char *x);
 int judgeOverflow(char num1[],char num2[]);
 int *judgeType(char x[],char y[]);
+struct FloatBigNum unToFloat(int type[],char x[],char y[]);
+struct FloatBigNum snToFloat(int type[],char x[],char y[]);
+struct SignedBigNum unToSigned(int type[],char x[],char y[]);
 
 #endif //HIGH_PRECISION_REAL_NUMBER_LIBRARY_UTILS_H
